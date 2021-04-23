95cc330d322c55f025512c499d7b229487ee2dc6
Yann Klis <yann.klis@gmail.com>
2021-04-23 11:29:38 +0200

    added product variations

--- lib/pipedrive/resources/product.rb
+++ lib/pipedrive/resources/product.rb
@@ -3,5 +3,21 @@
 module Pipedrive
   class Product < Resource
     include Fields
+
+    has_many :variations, class_name: "Variation"
+
+    # POST /prooducts/:id/variations
+    # Add a variation to this product
+    def add_variation(params)
+      raise "Param :name is required" unless params.key?(:name)
+      raise "Param :prices is required" unless params.key?(:prices)
+
+      response = request(
+        :post,
+        "#{resource_url}/variations",
+        params.merge(id: id)
+      )
+      Variation.new(response.dig(:data))
+    end
   end
 end
---
+++ lib/pipedrive/resources/variation.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pipedrive
+  class Variation < Resource
+    include Fields
+  end
+end
